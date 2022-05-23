150a4809efcb7c680bc2e88dbd22d9515e7ad13c
Joshua Ferriman <joshferriman@gmail.com>
2022-05-23 19:35:19 +0100

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+
+int main() 
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800,800, "Placeholder: SolarSystemSImGame", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window you fucking nerd" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+
+
+
+
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
